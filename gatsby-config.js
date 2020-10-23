74db72f99426d42bbfb91eb6ecaa9a454347b43b
freddneos <fredd@neosdev.com.br>
2020-10-23 01:26:27 +0100

    track code

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "G-MYHWRBT2MV",
+        trackingId: "UA-181153930-1",
         head: true,
       }
     },
